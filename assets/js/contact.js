83a42369eb73ecde69d083b4b6863bea895f6fc1
JMMelcrack <javiermm@correo.ugr.es>
2024-07-11 21:37:19 +0200

    Section contact added

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,27 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_udyfqxd', 'template_vlb2t83', '#contact-form', 'sPNUyvjr4LB_wN6hU')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
